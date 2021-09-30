adf5bd9fb1c0c58cb21813da385c8122b40ba2b4
tinoschroeter <mail@wgnetworks.de>
2021-09-30 18:08:40 +0200

    fix: data properties

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const app = express();
 app.get("/api", (req, res) => {
   const data = {};
 
-  data.i2 = Math.floor(Math.random() * 140 + 10);
-  data.i3 = Math.floor(Math.random() * 50);
-  data.i4 = Math.floor(Math.random() * 40);
-  data.i5 = Math.floor(Math.random() * 20);
-  data.requests = data.i2 + data.i3 + data.i4 + data.i5;
+  data.status_2xx = Math.floor(Math.random() * 140 + 10);
+  data.status_3xx = Math.floor(Math.random() * 50);
+  data.status_4xx = Math.floor(Math.random() * 40);
+  data.status_5xx = Math.floor(Math.random() * 20);
+  data.requests =
+    data.status_2xx + data.status_3xx + data.status_4xx + data.status_5xx;
 
   res.json(data);
 });
