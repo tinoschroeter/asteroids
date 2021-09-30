4e70d2276cbdbcd4aea84258f28539e52b06155d
tinoschroeter <mail@wgnetworks.de>
2021-09-30 17:44:23 +0200

    trigger: build pipeline

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,19 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
-app.get('/api', (req, res) => {
-const i2 = Math.floor(Math.random() * 140 + 10);
-const i3 = Math.floor(Math.random() * 50);
-const i4 = Math.floor(Math.random() * 40)
-const i5 = Math.floor(Math.random() * 20)
-const requests = i2 + i3 + i4 + i5
+app.get("/api", (req, res) => {
+  const data = {};
 
-    const data = {
-      status_2xx: i2,
-      status_3xx: i3,
-      status_4xx: i4,
-      status_5xx: i5,
-      requests: requests,
-    }
-    
-    res.json(data)
-})
+  data.i2 = Math.floor(Math.random() * 140 + 10);
+  data.i3 = Math.floor(Math.random() * 50);
+  data.i4 = Math.floor(Math.random() * 40);
+  data.i5 = Math.floor(Math.random() * 20);
+  data.requests = data.i2 + data.i3 + data.i4 + data.i5;
 
-const port = process.env.PORT || 3000
+  res.json(data);
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is runngin on Port ${port}`)
-})
+  console.log(`Server is runngin on Port ${port}`);
+});
